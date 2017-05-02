5adaa24575f3480c84b80ac7a3691e6ec3a4cdac
Etienne-Schmitt <etienne@schmitt-etienne.fr>
2017-05-02 19:59:22 +0200

    Optimisation

--- Source.c
+++ Source.c
@@ -13,9 +13,8 @@ pthread_t Reception, Transmission;
 pthread_mutex_t mutex;
 sem_t sem;
 
-
 struct sockaddr_rc loc_addr = {0}, rem_addr = {0};
-char buffer[32] = { 0 }, receive_addr[8] = { 0 };
+char buffer[32] = {0}, receive_addr[8] = {0};
 int sock, client, status;
 socklen_t opt = sizeof(rem_addr);
 
@@ -26,25 +25,53 @@ void main()
     printf("Starting Bluetooth Server\n");
     sem_init(&sem, 0, 0);
 
-    sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    if (pthread_create(&Reception, NULL, readInput, NULL) < 0)
+    {
+        perror("Thread:Reception error");
+        exit(1);
+    }
 
     loc_addr.rc_family = AF_BLUETOOTH;
     loc_addr.rc_bdaddr = *BDADDR_ANY;
-    loc_addr.rc_channel = (uint8_t) 1;
+    loc_addr.rc_channel = (uint8_t)1;
 
-    bind(sock, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+    sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    if (sock < 0)
+    {
+        perror("error socket");
+        exit(1);
+    }
 
-    listen(sock, 10);
+    if (bind(sock, (struct sockaddr *)&loc_addr, sizeof(loc_addr)) < 0)
+    {
+        perror("bind error");
+        exit(1);
+    }
 
-    pthread_create(&Reception, NULL, readInput, NULL);
-    pthread_create(&Transmission, NULL, sendOutput, NULL);
-
-    
+    if (listen(sock, 10) < 0)
+    {
+        perror("listen error");
+        exit(1);
+    }
 
     while (1)
     {
+        client = accept(sock, (struct sockaddr *)&rem_addr, &opt);
+        if (client < 0)
+        {
+            perror("accept error");
+            exit(1);
+        }
+
+        if (pthread_create(&Transmission, NULL, sendOutput, NULL) < 0)
+        {
+            perror("Thread:Transmission error");
+            exit(1);
+        }
+
         sleep(1);
     }
+
     close(client);
     close(sock);
 }
@@ -55,35 +82,15 @@ int *sendOutput()
     {
         sem_wait(&sem);
 
-        if (client = accept(sock, (struct sockaddr *)&rem_addr, &opt))
+        if (send(client, buffer, sizeof(buffer), 0) < 0)
         {
-            ba2str( &rem_addr.rc_bdaddr, receive_addr );
-            fprintf(stderr, "Connexion recu de : %s\n", receive_addr);
-
-            status = write(sock, "Test !", 6);
-            if (status < 0)
-            {
-                perror("Sending error");
-                exit(1);
-            }
-/*
-            while (1)
-            {            
-                sem_wait(&sem);
-                status = write(sock, "Test !", 6);
-
-                if (status < 0)
-                {
-                    perror("Sending error");
-                    exit(1);
-                }
-            }
-            */
+            perror("send error");
+            exit(1);
         }
 
-       
         printf("sendOutput() : buffer = %s\n", buffer);
     }
+
     pthread_exit(NULL);
     return 0;
 }
