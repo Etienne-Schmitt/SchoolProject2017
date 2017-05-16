c96341500b0f990edf21a10bd130269c02a54056
Etienne-Schmitt <etienne@schmitt-etienne.fr>
2017-05-16 22:03:56 +0200

    Updates

--- Source.c
+++ Source.c
@@ -59,7 +59,7 @@ int *sendOutput()
     {
         sem_wait(&sem);
 
-        if (send(sock, buffer, strlen(buffer) + 1, 0) < 0)
+        if (write(sock, buffer, strlen(buffer) + 1) < 0)
         {
             printf("Le client n'est pas connecter !\n");
         }
