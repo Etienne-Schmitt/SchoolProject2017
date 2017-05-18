ac875b9bac609c55f4221d6a799140431059274e
Etienne-Schmitt <etienne@schmitt-etienne.fr>
2017-05-18 16:42:56 +0200

    Changed : sockfd changed by sockeServer client changed by socketClient

--- Source.c
+++ Source.c
@@ -14,7 +14,7 @@ struct sockaddr_rc rem_addr = {0}, loc_addr = {0};
 pthread_mutex_t mutex;
 sem_t sem;
 char buffer[64] = {0};
-int sockfd, client;
+int socketServer, socketClient;
 char rc_addr[8] = {0};
 
 socklen_t length_rem_addr = sizeof(rem_addr);
@@ -32,20 +32,20 @@ void main()
 
     pthread_create(&Reception, NULL, readInput, NULL);
 
-    sockfd = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    socketServer = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
     loc_addr.rc_family = AF_BLUETOOTH;
     loc_addr.rc_bdaddr = *BDADDR_ANY;
     loc_addr.rc_channel = 10;
 
-    bind(sockfd, (struct sockaddr *)&loc_addr, length_loc_addr);
+    bind(socketServer, (struct sockaddr *)&loc_addr, length_loc_addr);
 
-    listen(sockfd, 1);
+    listen(socketServer, 1);
 
     printf("Attente de client\n");
     while (1)
     {
-        client = accept(sockfd, (struct sockaddr *)&rem_addr, &length_rem_addr);
+        socketClient = accept(socketServer, (struct sockaddr *)&rem_addr, &length_rem_addr);
 
         ba2str(&rem_addr.rc_bdaddr, rc_addr);
         printf("Connexion recu de : %s\n", rc_addr);
@@ -53,19 +53,19 @@ void main()
         pthread_create(&Transmission, NULL, sendOutput, NULL);
     }
 
-    close(client);
-    close(sockfd);
+    close(socketClient);
+    close(socketServer);
 }
 
 void *sendOutput()
 {
     printf("Thread envoie cree !\n");
-    while (client > 0)
+    while (socketClient > 0)
     {
         //pthread_mutex_lock(&mutex);
         sem_wait(&sem);
 
-        if (send(sockfd, buffer, (size_t)strlen(buffer) + 1, 0) < 0)
+        if (send(socketServer, buffer, sizeof(buffer), 0) < 0)
             printf("Le client c'est déconnecter !\n");
 
         //pthread_mutex_unlock(&mutex);
