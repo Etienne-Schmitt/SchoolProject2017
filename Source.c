34ab12b5fde845384cb4ac44b7a292b3687dce33
Etienne-Schmitt <etienne@schmitt-etienne.fr>
2017-05-18 14:17:17 +0200

    Multi client listen

--- Source.c
+++ Source.c
@@ -10,6 +10,8 @@
 
 #include "RS232.h"
 
+#define CHANNEL 10
+
 struct sockaddr_rc rem_addr = {0};
 pthread_mutex_t mutex;
 sem_t sem;
@@ -25,7 +27,8 @@ void main()
     struct sockaddr_rc loc_addr = {0};
     char receive_addr[8] = {0};
     int client;
-    socklen_t opt = sizeof(rem_addr);
+    socklen_t length_rem_addr = sizeof(rem_addr);
+    socklen_t length_loc_addr = sizeof(loc_addr);
 
     printf("Starting Bluetooth Server\n");
     sem_init(&sem, 0, 0);
@@ -35,17 +38,17 @@ void main()
 
     loc_addr.rc_family = AF_BLUETOOTH;
     loc_addr.rc_bdaddr = *BDADDR_ANY;
-    loc_addr.rc_channel = (uint8_t)1;
+    loc_addr.rc_channel = (uint8_t)CHANNEL;
 
     sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    bind(sock, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
-
-    listen(sock, 10);
+    bind(sock, (struct sockaddr *)&loc_addr, length_loc_addr);
 
     while (1)
     {
-        client = accept(sock, (struct sockaddr *)&rem_addr, &opt);
+        listen(sock, CHANNEL);
+
+        client = accept(sock, (struct sockaddr *)&rem_addr, &length_rem_addr);
 
         // Conversion de l'adresse BT en string (char array) dans receive_addr
         ba2str(&rem_addr.rc_bdaddr, receive_addr);
@@ -62,14 +65,13 @@ void *sendOutput()
 {
     while (client)
     {
-    pthread_mutex_lock(&mutex);
-    sem_wait(&sem);
+        pthread_mutex_lock(&mutex);
+        sem_wait(&sem);
 
-    if (send(client, buffer, (size_t) strlen(buffer) + 1, 0) < 0)
-        printf("Le client n'est pas connecter !\n");
+        if (send(client, buffer, (size_t)strlen(buffer) + 1, 0) < 0)
+            printf("Le client n'est pas connecter !\n");
 
-    pthread_mutex_unlock(&mutex);
-
+        pthread_mutex_unlock(&mutex);
     }
     pthread_exit(NULL);
 }
